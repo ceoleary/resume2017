d28fd1bcf9f23b314fcff1e2d69080a80f4966ab
Catie O'Leary <ceokateri@gmail.com>
2017-02-20 14:11:59 -0500

    new nav bar

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,36 @@
 $(document).ready(function(){
+  //if debug mode, true
+  var debug = true;
+  //rgb colors
+  var rgbNav = "238,110,115";
+
     $('.scrollspy').scrollSpy();
     $('#textarea1').trigger('autoresize');
     $('.carousel').carousel();
+
+    $(window).scroll(function(){
+      if($(this).scrollTop() > 5 && $(this).scrollTop() < 100){
+        var scrollNum = $(this).scrollTop();
+        var a = scrollNum/50;
+        $('nav').css({"background-color":"rgba("+rgbNav+","+a+")"});
+        log("transition " + a);
+      }
+      else if($(this).scrollTop() > 5)
+      {
+        //When the navbar is not at the top
+         $('nav').css({"background-color":"rgba("+rgbNav+",1)"});
+         log("scroll top");
+      }
+      else {
+        //When the navbar is on the top
+         $('nav').css({"background-color":"rgba("+rgbNav+",0)"});
+         log("else");
+      }
+    });
+
+    function log(text){
+      if(debug == true){
+        console.log(text);
+      }
+    }
   });
